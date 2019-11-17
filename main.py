bc66fdff8b437c989056076af160dc5ab76950b5
sail004 <55446758+sail004@users.noreply.github.com>
2019-11-17 21:31:31 +0500

    создан интерфейс

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
 
- 
-class ConvertService(QWidget):
+class Settings(QDialog):
     def __init__(self):
         super().__init__()
-        self.initUI()
-        
-    def initUI(self):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Конверт Сервис')    
+        uic.loadUi('settings.ui', self)
+
+
+class ConvertService(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('mainwindow.ui', self)
+        self.settings_open_button.clicked.connect(self.show_settings)
+
+    def show_settings(self):
+        settingsWindow = Settings()
+        settingsWindow.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = ConvertService()
     ex.show()
     sys.exit(app.exec())  
+
+
