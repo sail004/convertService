9bbd8f301fe2c83c2e0279b7caf574ee6b185020
sail004 <55446758+sail004@users.noreply.github.com>
2019-11-24 12:09:32 +0500

    тест

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
+import json
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
 
 class Settings(QDialog):
-    def __init__(self):
+    def __init__(self, settings):
         super().__init__()
         uic.loadUi('settings.ui', self)
+        self.settings=settings
 
 
 class ConvertService(QMainWindow):
@@ -13,7 +15,15 @@ class ConvertService(QMainWindow):
         super().__init__()
         uic.loadUi('mainwindow.ui', self)
         self.settings_open_button.clicked.connect(self.show_settings)
-        self.settingsWindow = Settings()
+        with open("settings.json", "r") as read_file:
+            self.settings = json.load(read_file)
+        self.settingsWindow = Settings(self.settings)
+        pushButton.clicked.connect.self.run()
+
+    def run(self):
+        self.dbname = db_way.text()
+        self.settings = dbname
+        
 
     def show_settings(self):
         self.settingsWindow.show()
