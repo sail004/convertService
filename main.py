8c3bac87a55801b861b30ad8fc19aca581859f42
sail004 <55446758+sail004@users.noreply.github.com>
2020-05-09 12:42:10 +0500

    поддержка xml

--- main.py
+++ main.py
@@ -2,35 +2,13 @@ import sys
 import json
 import os.path
 import sqlite3
+import constants
 from PyQt5 import uic
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
+import settings
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
 
-class Settings(QDialog):
-    def __init__(self, settings):
-        super().__init__()
-        uic.loadUi('settings.ui', self)
-        self.settings = settings
-        self.pushButton.clicked.connect(self.run)
-        self.db_way.setText(self.settings["dbPath"])
-        self.setModal(True)
-
-    def run(self):
-        if (os.path.exists(self.db_way.text())):
-            self.settings["dbPath"] = self.db_way.text()
-            with open("settings.json", "w") as read_file:
-                json.dump(self.settings, read_file)
-            self.close()
-            con = sqlite3.connect(self.settings["dbPath"] )
-            cur = con.cursor()
-            result = cur.execute(self.sql_request.text()).fetchall()
-            print(result)
-        else:
-            error_dialog = QtWidgets.QErrorMessage()
-            error_dialog.setModal(True)
-            error_dialog.showMessage('Ошибка! Нет такого файла в системе.')
-            error_dialog.exec_()
 
 class ConvertService(QMainWindow):
     def __init__(self):
@@ -38,12 +16,12 @@ class ConvertService(QMainWindow):
         uic.loadUi('mainwindow.ui', self)
         self.settings_open_button.clicked.connect(self.show_settings)
         fileName = "settings.json"
-        self.settings = {}
+        self.appSettings = {}
         if (os.path.exists(fileName)):
             with open(fileName, "r") as read_file:
-                self.settings = json.load(read_file)
+                self.appSettings = json.load(read_file)
 
-        self.settingsWindow = Settings(self.settings)
+        self.settingsWindow = settings.Settings(self.appSettings)
 
     def show_settings(self):
         self.settingsWindow.show()
