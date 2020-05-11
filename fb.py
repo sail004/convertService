ac44f33a1053fb8cf09d7f4ee7f080f7f9e28695
An <zksn8z@gmail.com>
2020-05-11 23:51:01 +0500

    fb test

--- fb.py
+++ fb.py
@@ -1,3 +1,12 @@
 import fdb
 
-con = fdb.connect(dsn='localhost:axioma', user='sysdba', password='pass')
+con = fdb.connect(dsn='localhost:axioma', user='sysdba', password='masterke')
+
+# Create a Cursor object that operates in the context of Connection con:
+cur = con.cursor()
+
+# Execute the SELECT statement:
+cur.execute("select first 10 * from assortment")
+
+# Retrieve all rows as a sequence and print that sequence:
+print(cur.fetchall())
